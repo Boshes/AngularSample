86a11b6acd14b69e42492a1e64b26c8ddacc7f04
Justen Morgan <justenamorgan@gmail.com>
2015-06-26 11:26:38 -0700

    ???

--- javascript.js
+++ javascript.js
@@ -1,131 +1,162 @@
-var app= angular.module("app",['ui.bootstrap', 'ngDragDrop','dndLists']);
+var app = angular.module("app", ['ui.bootstrap', 'ngDragDrop', 'dndLists']);
 
-app.controller('TabsCtrl', function($scope){
-  $scope.tabs = [ {title: 'Tab 2', content: 'Tab content 2'},
-                              {title: 'Tab 3', content: 'Tab content 3'}];
-  $scope.firstname;
-  $scope.num
-  $scope.str;
+app.controller('TabsCtrl', function($scope) {
+        $scope.tabs = [{
+                title: 'Tab 2',
+                content: 'Tab content 2'
+        }, {
+                title: 'Tab 3',
+                content: 'Tab content 3'
+        }];
+        $scope.firstname;
+        $scope.num
+        $scope.str;
 });
-app.factory('messages',function(){
-  var messages = {};
- messages.list = [];
- messages.add = function(message){
-   messages.list.push({id: messages.list.length, text: message});
- };
- return messages;
+app.factory('messages', function() {
+        var messages = {};
+        messages.list = [];
+        messages.add = function(message) {
+                messages.list.push({
+                        id: messages.list.length,
+                        text: message
+                });
+        };
+        return messages;
 });
-app.controller('ListCtrl', function(messages){
-  var self = this;
-   self.messages = messages.list;
+app.controller('ListCtrl', function(messages) {
+        var self = this;
+        self.messages = messages.list;
 });
-app.controller('PostCtrl', function (messages){
- var self = this;
- self.newMessage = 'Hello World!';
- self.addMessage = function(message){
-   messages.add(message);
-   self.newMessage = '';
- };
+app.controller('PostCtrl', function(messages) {
+        var self = this;
+        self.newMessage = 'Hello World!';
+        self.addMessage = function(message) {
+                messages.add(message);
+                self.newMessage = '';
+        };
 });
 
-app.factory('getData', getData).run(function(getData){
-  var promise = getData()
-  .then(function(num){
-    console.log(num);
-    return num *2})
-    .then(function(num){
-      console.log(num)
-    });
-  });
+app.factory('getData', getData).run(function(getData) {
+        var promise = getData()
+                .then(function(num) {
+                        console.log(num);
+                        return num * 2
+                })
+                .then(function(num) {
+                        console.log(num)
+                });
+});
 
-function getData($timeout, $q){
-  return function() {
-    return $q(function(resolve, reject){
-      $timeout(function(){
-        resolve(Math.floor(Math.random()*10))
-      },2000);
-    });
-  };
+function getData($timeout, $q) {
+        return function() {
+                return $q(function(resolve, reject) {
+                        $timeout(function() {
+                                resolve(Math.floor(Math.random() * 10))
+                        }, 2000);
+                });
+        };
 };
 
-app.directive('welcomee', function(){
-  return{
-    restrict: "E",
-    template: "<div>Welcome Element!</div>"
-  };
+app.directive('welcomee', function() {
+        return {
+                restrict: "E",
+                template: "<div>Welcome Element!</div>"
+        };
 });
-app.directive('welcomea', function(){
-  return{
-    restrict: "A",
-    link: function(){
-      alert("Welcome Attribute!!");
-    }
-  };
+app.directive('welcomea', function() {
+        return {
+                restrict: "A",
+                link: function() {
+                        alert("Welcome Attribute!!");
+                }
+        };
 });
-app.directive('goodbyea', function(){
-  return{
-    restrict: "A",
-    link: function(){
-      alert("Later!");
-    }
-  };
+app.directive('goodbyea', function() {
+        return {
+                restrict: "A",
+                link: function() {
+                        alert("Later!");
+                }
+        };
 });
-app.directive('welcomec',function(){
-  return{
-    restrict: "C",
-    link: function(){
-      alert("Welcome Class!!!");
-    }
-  };
+app.directive('welcomec', function() {
+        return {
+                restrict: "C",
+                link: function() {
+                        alert("Welcome Class!!!");
+                }
+        };
 });
-app.directive('welcomem', function(){
-  return{
-    restrict: "M",
-    link: function(){
-      alert("Welcome Comment!!!!");
-    }
-  };
+app.directive('welcomem', function() {
+        return {
+                restrict: "M",
+                link: function() {
+                        alert("Welcome Comment!!!!");
+                }
+        };
 });
 app.controller('oneCtrl', function($scope, $timeout) {
-  $scope.list1 = [];
-  $scope.list2 = [];
-  $scope.list3 = [];
-  $scope.list4 = [];
-  $scope.list5 = [
-    { 'title': 'Item 1', 'drag': true },
-    { 'title': 'Item 2', 'drag': true },
-    { 'title': 'Item 3', 'drag': true },
-    { 'title': 'Item 4', 'drag': true },
-    { 'title': 'Item 5', 'drag': true },
-    { 'title': 'Item 6', 'drag': true },
-    { 'title': 'Item 7', 'drag': true },
-    { 'title': 'Item 8', 'drag': true }
-  ];
-  // Limit items to be dropped in list1
-  $scope.optionsList1 = {
-    accept: function(dragEl) {
-      if ($scope.list1.length >= 2) {
-        return false;
-      } else {
-        return true;
-      }
-    }
-  };
+        $scope.list1 = [];
+        $scope.list2 = [];
+        $scope.list3 = [];
+        $scope.list4 = [];
+        $scope.list5 = [{
+                'title': 'Item 1',
+                'drag': true
+        }, {
+                'title': 'Item 2',
+                'drag': true
+        }, {
+                'title': 'Item 3',
+                'drag': true
+        }, {
+                'title': 'Item 4',
+                'drag': true
+        }, {
+                'title': 'Item 5',
+                'drag': true
+        }, {
+                'title': 'Item 6',
+                'drag': true
+        }, {
+                'title': 'Item 7',
+                'drag': true
+        }, {
+                'title': 'Item 8',
+                'drag': true
+        }];
+        // Limit items to be dropped in list1
+        $scope.optionsList1 = {
+                accept: function(dragEl) {
+                        if ($scope.list1.length >= 2) {
+                                return false;
+                        } else {
+                                return true;
+                        }
+                }
+        };
 });
-app.controller('dndCtrl',function($scope){
-  $scope.models = {
-       selected: null,
-       lists: {"A": [], "B": []}
-   };
+app.controller('dndCtrl', function($scope) {
+        $scope.models = {
+                selected: null,
+                lists: {
+                        "A": [],
+                        "B": []
+                }
+        };
 
-   // Generate initial model
-   for (var i = 1; i <= 3; ++i) {
-       $scope.models.lists.A.push({label: "Item A" + i});
-       $scope.models.lists.B.push({label: "Item B" + i});
-   }
+        // Generate initial model
+        for (var i = 1; i <= 3; ++i) {
+                $scope.models.lists.A.push({
+                        label: "Item A" + i
+                });
+                $scope.models.lists.B.push({
+                        label: "Item B" + i
+                });
+        }
 
-   // Model to JSON for demo purpose
-   $scope.$watch('models', function(model) {
-       $scope.modelAsJson = angular.toJson(model, true);
-   }, true);
+        // Model to JSON for demo purpose
+        $scope.$watch('models', function(model) {
+                $scope.modelAsJson = angular.toJson(model, true);
+        }, true);
 });
--- test.js
+++ test.js
@@ -1,192 +1,186 @@
-var test = angular.module('test',['dndLists','ui.bootstrap']);
+var test = angular.module('test', ['dndLists', 'ui.bootstrap']);
 
-test.controller('testCtrl',function($scope){
-      $scope.models = {
-          selected: null,
-          lists: {"A": [{"label":"Derp"},{"label":"Herp"}], "B": []}
-      };
-      for (var i = 1; i <= 3; ++i ) {
-          $scope.models.lists.A.push({label: "Item A" + i});
-          $scope.models.lists.B.push({label: "Item B" + i});
-      }
-  });
+test.controller('testCtrl', function($scope) {
+        $scope.models = {
+                selected: null,
+                lists: {
+                        "A": [{
+                                "label": "Derp"
+                        }, {
+                                "label": "Herp"
+                        }],
+                        "B": []
+                }
+        };
+        for (var i = 1; i <= 3; ++i) {
+                $scope.models.lists.A.push({
+                        label: "Item A" + i
+                });
+                $scope.models.lists.B.push({
+                        label: "Item B" + i
+                });
+        }
+});
 
-  test.controller('NestedDemo',function($scope){
-  $scope.models = {
-          selected: null,
-          templates: [
-              {type: "item", id: 2},
-              {type: "container", id: 1, columns: [[], []]},
-              {type: "value",id: 3, last: [[],[],[]]}
-          ],
-          dropzones: {
-           "A": [
-               {
-                   "type": "container",
-                   "id": 1,
-                   "columns": [
-                       [
-                           {
-                               "type": "item",
-                               "id": "1",
-                               "last":[
-                                   {
-                                       "type":"value",
-                                       "id":"3"
-                                   }
-                               ]
-                           }
-                       ],
-                           [
-                           {
-                               "type": "item",
-                               "id": "2"
-                           }
-                       ],
-                       [
-                           {
-                               "type": "item",
-                               "id": "3"
-                           }
-                       ]
-                   ]
-               },
-               {
-                   "type": "item",
-                   "id": "4"
-               },
-               {
-                   "type": "item",
-                   "id": "5"
-               },
-               {
-                   "type": "item",
-                   "id": "6"
-               }
-           ],
-           "B": [
-               {
-                   "type": "item",
-                   "id": 7
-               },
-               {
-                   "type": "item",
-                   "id": "8"
-               },
-               {
-                   "type": "container",
-                   "id": "2",
-                   "columns": [
-                       [
-                           {
-                               "type": "item",
-                               "id": "9"
-                           },
-                           {
-                               "type": "item",
-                               "id": "10"
-                           },
-                           {
-                               "type": "item",
-                               "id": "11"
-                           }
-                       ],
-                       [
-                           {
-                               "type": "item",
-                               "id": "12"
-                           },
-                           {
-                               "type": "container",
-                               "id": "3",
-                               "columns": [
-                                   [
-                                       {
-                                           "type": "item",
-                                           "id": "13"
-                                       }
-                                   ],
-                                   [
-                                       {
-                                           "type": "item",
-                                           "id": "14"
-                                       }
-                                   ]
-                               ]
-                           },
-                           {
-                               "type": "item",
-                               "id": "15"
-                           },
-                           {
-                               "type": "item",
-                               "id": "16"
-                           }
-                       ]
-                   ]
-               },
-               {
-                   "type": "item",
-                   "id": 16
-               }
-           ],
-           "C":[
-               {"type":"item",
-                "id":"1"}
-           ]
-       }
-      };
-  });
+test.controller('NestedDemo', function($scope) {
+        $scope.models = {
+                selected: null,
+                templates: [{
+                        type: "item",
+                        id: 2
+                }, {
+                        type: "container",
+                        id: 1,
+                        columns: [
+                                [],
+                                []
+                        ]
+                }, {
+                        type: "value",
+                        id: 3,
+                        last: [
+                                [],
+                                [],
+                                []
+                        ]
+                }],
+                dropzones: {
+                        "A": [{
+                                "type": "container",
+                                "id": 1,
+                                "columns": [
+                                        [{
+                                                "type": "item",
+                                                "id": "1",
+                                                "last": [{
+                                                        "type": "value",
+                                                        "id": "3"
+                                                }]
+                                        }],
+                                        [{
+                                                "type": "item",
+                                                "id": "2"
+                                        }],
+                                        [{
+                                                "type": "item",
+                                                "id": "3"
+                                        }]
+                                ]
+                        }, {
+                                "type": "item",
+                                "id": "4"
+                        }, {
+                                "type": "item",
+                                "id": "5"
+                        }, {
+                                "type": "item",
+                                "id": "6"
+                        }],
+                        "B": [{
+                                "type": "item",
+                                "id": 7
+                        }, {
+                                "type": "item",
+                                "id": "8"
+                        }, {
+                                "type": "container",
+                                "id": "2",
+                                "columns": [
+                                        [{
+                                                "type": "item",
+                                                "id": "9"
+                                        }, {
+                                                "type": "item",
+                                                "id": "10"
+                                        }, {
+                                                "type": "item",
+                                                "id": "11"
+                                        }],
+                                        [{
+                                                "type": "item",
+                                                "id": "12"
+                                        }, {
+                                                "type": "container",
+                                                "id": "3",
+                                                "columns": [
+                                                        [{
+                                                                "type": "item",
+                                                                "id": "13"
+                                                        }],
+                                                        [{
+                                                                "type": "item",
+                                                                "id": "14"
+                                                        }]
+                                                ]
+                                        }, {
+                                                "type": "item",
+                                                "id": "15"
+                                        }, {
+                                                "type": "item",
+                                                "id": "16"
+                                        }]
+                                ]
+                        }, {
+                                "type": "item",
+                                "id": 16
+                        }],
+                        "C": [{
+                                "type": "item",
+                                "id": "1"
+                        }]
+                }
+        };
+});
 
-  test.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
-          $scope.result = [];
-          $scope.firstnamelabel = "First Name";
-          $scope.firstname;
-          $scope.lastnamelabel = "Last Name";
-          $scope.lastname;
-          $scope.submit = function(){
-                  if ($scope.firstname){
-                          $scope.result.push(this.firstnamelabel + "':'" + this.firstname);
-                          $scope.firstname = '';
-                  }
-                  if ($scope.lastname){
-                         $scope.result.push(this.lastnamelabel +"':'" + this.lastname);
-                          $scope.lastname = '';
-                  }
-          };
-  $scope.animationsEnabled = true;
+test.controller('ModalDemoCtrl', function($scope, $modal, $log) {
+        $scope.result = [];
+        $scope.firstnamelabel = "First Name";
+        $scope.firstname;
+        $scope.lastnamelabel = "Last Name";
+        $scope.lastname;
+        $scope.submit = function() {
+                if ($scope.firstname) {
+                        $scope.result.push(this.firstnamelabel + "':'" + this.firstname);
+                        $scope.firstname = '';
+                }
+                if ($scope.lastname) {
+                        $scope.result.push(this.lastnamelabel + "':'" + this.lastname);
+                        $scope.lastname = '';
+                }
+        };
+        $scope.animationsEnabled = true;
 
-  $scope.open = function (size) {
+        $scope.open = function(size) {
 
-    var modalInstance = $modal.open({
-      animation: $scope.animationsEnabled,
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl',
-      size: size,
-      resolve: {
-        result: function () {
-          return $scope.result;
-  }
-    }
-    });
-};
-  $scope.toggleAnimation = function () {
-    $scope.animationsEnabled = !$scope.animationsEnabled;
-  };
+                var modalInstance = $modal.open({
+                        animation: $scope.animationsEnabled,
+                        templateUrl: 'myModalContent.html',
+                        controller: 'ModalInstanceCtrl',
+                        size: size,
+                        resolve: {
+                                result: function() {
+                                        return $scope.result;
+                                }
+                        }
+                });
+        };
+        $scope.toggleAnimation = function() {
+                $scope.animationsEnabled = !$scope.animationsEnabled;
+        };
 
 });
 
 // Please note that $modalInstance represents a modal window (instance) dependency.
 // It is not the same as the $modal service used above.
 
-test.controller('ModalInstanceCtrl', function ($scope, $modalInstance,result) {
+test.controller('ModalInstanceCtrl', function($scope, $modalInstance, result) {
 
         $scope.result = result;
-  $scope.ok = function () {
-    $modalInstance.dismiss();
-  };
+        $scope.ok = function() {
+                $modalInstance.dismiss();
+        };
 
-  $scope.cancel = function () {
-    $modalInstance.dismiss();
-  };
+        $scope.cancel = function() {
+                $modalInstance.dismiss();
+        };
 });
